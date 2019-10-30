d8576a787891f6a30dd6b4eb80567c8b66de1b82
KatyaBaranovskaya <katyabaranovskaya97@gmail.com>
2019-10-30 12:06:13 +0300

    add header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
 
 function App() {
   return (
     <div className="app">
+      <Header className="app__header"/>
+      <div className="app__content"/>
+      <Footer className="app__footer"/>
     </div>
   );
 }
---
+++ src/footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTwitter, faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons'
+
+import './Footer.css';
+
+const ICONS = [faTwitter, faFacebook, faInstagram];
+
+function Footer({className}) {
+  return (
+    <footer className={classNames('footer', className)}>
+      <p className="footer__copy">&copy; 2018 K. Baranouskaya</p>
+      <ul className="footer__icons">
+        {ICONS.map((icon, index) => (
+          <li key={index} className="footer__list-item">
+            <a className="footer__icon-link" href=""><FontAwesomeIcon icon={icon}/></a>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
